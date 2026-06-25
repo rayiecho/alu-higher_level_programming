898de297caf0b4454399b913e277d42ee005291d
rayiecho <r.ayiecho@alustudent.com>
2026-06-24 22:53:26 -0700

    Move task 6 to correct directory python-hello_world

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Concatenate two strings to form a welcome message
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to " + str1 + " " + str2 + "!")
+str3 = str1 + " " + str2
+print("Welcome to " + str3 + "!")
