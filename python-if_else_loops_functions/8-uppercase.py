8df2a09e7a7950244babe41e031352ed3cb8861c
rayiecho <r.ayiecho@alustudent.com>
2026-06-24 23:32:15 -0700

    Task 9: reduce to 2 print statements in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        n = ord(c)
+        if n >= ord('a') and n <= ord('z'):
+            n = n - 32
+        print("{}".format(chr(n)), end="")
     print("")
