3e981c073f1d8ff7fb61e7fddd7ce7a98d1e77f2
rayiecho <r.ayiecho@alustudent.com>
2026-06-24 23:09:36 -0700

    Task 8: restore 8/9 working version

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language. It mixes ideas from Modula-3, C, and it is often compared to Java. It supports multiple programming paradigms beyond object-oriented programming, such as procedural and functional programming. Python combines remarkable power with very clear syntax."
-print(str[39:66] + str[302:308] + str[0:6])
+str = "Python is an interpreted, interactive, object-oriented programming language. It mixes ideas from Modula-3, C, ..."
+print(str[39:66] + str[68:74] + str[37:38] + str[97:106])
