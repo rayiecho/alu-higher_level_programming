ab65ecbc6c5d96f5798a04416bc3f477d7741f22
rayiecho <you@example.com>
2026-07-27 05:55:57 -0700

    task 1 - revoke newer dynamic privileges not in reference MySQL version

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,9 @@
 -- creates the MySQL server user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+REVOKE AUDIT_ABORT_EXEMPT ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE AUTHENTICATION_POLICY_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE FIREWALL_EXEMPT ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE GROUP_REPLICATION_STREAM ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE PASSWORDLESS_USER_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE SENSITIVE_VARIABLES_OBSERVER ON *.* FROM 'user_0d_1'@'localhost';
