806b989f916f9dbadcbd31d00541551cd65187ca
rayiecho <you@example.com>
2026-07-31 09:33:35 -0700

    task 0 - fix domain to intranet.hbtn.io

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status and displays the response body."""
+"""Fetches https://intranet.hbtn.io/status and displays the response body."""
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
